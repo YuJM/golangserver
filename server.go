01b960c65435fc66fd26d71581ed912fd73f35d9
YuJM <zsxzsx1010@gmail.com>
2014-01-29 12:13:47 +0900

    com

--- server.go
+++ server.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"html/template"
 	"net/http"
 	// "path/filepath"
 )
 
 func thandle(rw http.ResponseWriter, req *http.Request) {
-	t, _ := template.ParseFiles("./test2.html")
+	t, _ := template.ParseFiles("./test.html")
 	t.Execute(rw, nil)
 }
 
 func main() {
 
 	http.HandleFunc("/", thandle)
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 
-	http.ListenAndServe("127.0.0.1:9999", nil)
+	http.ListenAndServe("127.0.0.1:9999", func() http.Handler {
+		fmt.Println("server Running ")
+		return nil
+	}())
 
 }
